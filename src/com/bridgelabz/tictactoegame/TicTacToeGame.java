767df71bc4818c53dca136108bc1b11eb0c0fa10
Naveen Pattanashetti <pattanashettinaveen.com>
2021-09-30 08:59:04 +0530

    added:program to create the empty board for TicTacToeGame

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -3,9 +3,18 @@ package com.bridgelabz.tictactoegame;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
-	 System.out.println("TicTacToeGame Program");
-	 
+		System.out.println("TicTacToeGame Program");
+		TicTacToeGame tictactoegame = new TicTacToeGame();
+		tictactoegame.ticTacToeboard();
 	}
- 
 
+	private void ticTacToeboard() {
+		char[] elements = new char[10];
+		for (int i = 1; i < 10; i++) {
+
+			elements[i] = ' ';
+			System.out.println(elements[i]);
+		}
+		System.out.println("Created Empty Board");
+	}
 }
